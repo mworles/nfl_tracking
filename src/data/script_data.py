e9a31de48e9ed932e0acc787f4d3cf0aed9af6ab
Matthew Worley <mworles@gmail.com>
2019-01-10 12:21:10 -0800

    update data processing script

--- src/data/script_data.py
+++ src/data/script_data.py
@@ -1,7 +1,7 @@
 # import data
-import os
-d = 'c:/users/mworley/nfl_tracking/src/data/'
-os.chdir(d)
-#import d + get_rushes
-#import clean_rushes
+#import os
+#d = 'c:/users/mworley/nfl_tracking/src/data/'
+#os.chdir(d)
+#import get_rushes
+import clean_rushes
 import get_defenders
